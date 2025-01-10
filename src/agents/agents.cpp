daff7f31d0cfa3230fefa435adb797e196d17b5f
Henry Li <henry1943@163.com>
2025-01-10 14:44:13 +0800

    chore: add agents and avatars

---
+++ src/agents/agents.cpp
@@ -0,0 +1,39 @@
+#include "agents.h"
+
+#include <lvgl.h>
+
+LV_IMAGE_DECLARE(img_avatar_amy);
+LV_IMAGE_DECLARE(img_avatar_lisa);
+LV_IMAGE_DECLARE(img_avatar_sam);
+LV_IMAGE_DECLARE(img_avatar_tony);
+
+Agent agent_amy = {
+    "amy",
+    "Amy",
+    &img_avatar_amy,
+};
+
+Agent agent_lisa = {
+    "lisa",
+    "Lisa",
+    &img_avatar_lisa,
+};
+
+Agent agent_sam = {
+    "sam",
+    "Sam",
+    &img_avatar_sam,
+};
+
+Agent agent_tony = {
+    "tony",
+    "Tony",
+    &img_avatar_tony,
+};
+
+const Agent agents[] = {
+    agent_amy,
+    agent_lisa,
+    agent_sam,
+    agent_tony,
+};
---
+++ src/agents/agents.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <Arduino.h>
+
+struct Agent {
+  const char* agentId;
+  const char* name;
+  const void* avatar;
+};
+
+extern Agent agent_amy;
+extern Agent agent_lisa;
+extern Agent agent_sam;
+extern Agent agent_tony;
+
+extern const Agent agents[4];
